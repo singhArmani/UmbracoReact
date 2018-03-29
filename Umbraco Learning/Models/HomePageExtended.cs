fa7325b87c120afe1514e682c8ad70efb827625d
4MATION\amandeep.singh <amandeep.singh@4mation.com.au>
2018-03-29 17:44:16 +1100

    AppData mode added

---
+++ Umbraco Learning/Models/HomePageExtended.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+
+namespace Umbraco_Learning.Models
+{
+    public class HomePageExtended : Umbraco.Web.PublishedContentModels.HomePage
+    {
+        public HomePageExtended(IPublishedContent content) : base(content)
+        {
+        }
+        public string Number { get { return "trest"; } }
+    }
+}
